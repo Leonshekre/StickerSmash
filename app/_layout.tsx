774f13dc3e269a57a1c30297291bab28051fce7f
Leonardo Paredes <leonaa2@uw.edu>
2025-02-15 12:59:40 -0800

    Finished Expo tutorial, going to EAS build tutorial, (finished Configure, learning resources)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack> //* How does (tabs) -> (tabs dir) function?
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
